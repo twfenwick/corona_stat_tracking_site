7630b263f57803ccfbb673809882eaec52bede47
tim fenwick <twfenwick@gmail.com>
2020-05-07 01:38:34 -0400

    comment out configs

--- after_install.sh
+++ after_install.sh
@@ -9,10 +9,10 @@ pip install -r requirements.txt
 #python manage.py collectstatic --no-input
 
 # Set permission for all files
-sudo chown -R www-data:www-data /var/www/
+#sudo chown -R www-data:www-data /var/www/
 
 # Restart services
-sudo -Hu www-data chmod a+x /var/www/backend/server_configs/scripts/gunicorn_django.sh
-sudo service supervisor restart
-sudo service nginx restart
+#sudo -Hu www-data chmod a+x /var/www/backend/server_configs/scripts/gunicorn_django.sh
+#sudo service supervisor restart
+#sudo service nginx restart
 #python manage.py runserver
