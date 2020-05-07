aef4267390b26ef9b1c350b9f56213175121fb94
tim fenwick <twfenwick@gmail.com>
2020-05-06 21:46:55 -0400

    build spec start server

--- after_install.sh
+++ after_install.sh
@@ -5,8 +5,8 @@ cd /var/www/backend
 virtualenv -p python3 venv
 source venv/bin/activate
 pip install -r requirements.txt
-python manage.py migrate
-python manage.py collectstatic --no-input
+#python manage.py migrate
+#python manage.py collectstatic --no-input
 
 # Set permission for all files
 sudo chown -R www-data:www-data /var/www/
@@ -14,4 +14,5 @@ sudo chown -R www-data:www-data /var/www/
 # Restart services
 sudo -Hu www-data chmod a+x /var/www/backend/server_configs/scripts/gunicorn_django.sh
 sudo service supervisor restart
-sudo service nginx restart
+#sudo service nginx restart
+python manage.py runserver
