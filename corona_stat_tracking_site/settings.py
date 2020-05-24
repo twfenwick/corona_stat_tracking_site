9d6932fe5aab25a7b1efcac345e36345c4ffb4d9
tim fenwick <twfenwick@gmail.com>
2020-05-24 14:13:33 -0400

    edit settings.py

--- corona_stat_tracking_site/settings.py
+++ corona_stat_tracking_site/settings.py
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = ['corona-track-env.eba-5kd25yp2.us-east-1.elasticbeanstalk.com',
 # Application definition
 
 INSTALLED_APPS = [
+    'dailystats.apps.DailyStatsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dailystats.apps.DailyStatsConfig',
 ]
 
 MIDDLEWARE = [
