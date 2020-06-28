8ab890c77d29226d27a3541a096a4e28657196d1
tim fenwick <twfenwick@gmail.com>
2020-06-28 11:58:13 -0400

    Add models run migration to update sqlite3 db

--- dailystats/models.py
+++ dailystats/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class State(models.Model):
+    state_name = models.CharField(max_length=50)
+    state_code = models.CharField(max_length=2)
+
+
+class County(models.Model):
+    county_name = models.CharField(max_length=100)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
